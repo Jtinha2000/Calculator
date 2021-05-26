9037a5efe31869b05e08ab4186e9659e287d1f1d
CommunistDoggu <77682013+Jtinha2000@users.noreply.github.com>
2021-05-26 10:49:19 -0300

    Applications
    Applications

---
+++ Calculator/Calculator/Maty.cs
@@ -0,0 +1,79 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp17
+{
+    class Maty
+    {
+        private string Response1;
+        public double Number { get; private set; }
+        public Maty(double number, string response1)
+        {
+            Response1 = response1;
+            Number = number;
+        }
+        public double count()
+        {
+            if (Response1.Contains("+"))
+            {
+                string[] response2 = Response1.Split('+');
+                Number = double.Parse(response2[0]) + double.Parse(response2[1]);
+            }
+            else if (Response1.Contains("-"))
+            {
+                string[] response2 = Response1.Split('-');
+                Number = double.Parse(response2[0]) - double.Parse(response2[1]);
+            }
+            else if (Response1.Contains("*"))
+            {
+                string[] response2 = Response1.Split('*');
+                Number = double.Parse(response2[0]) * double.Parse(response2[1]);
+            }
+            else if (Response1.Contains("/"))
+            {
+                string[] response2 = Response1.Split('/');
+                Number = double.Parse(response2[0]) / double.Parse(response2[1]);
+            }
+            else
+            {
+                Console.WriteLine("Enter a correct value...");
+            }
+            return Number;
+        }
+        public double count1(string Response2)
+        {
+            if (Response2.Contains("+"))
+            {
+                string[] Response6 = Response2.Split('+');
+                Number = Number + double.Parse(Response6[1]);
+            }
+            else if (Response2.Contains("-"))
+            {
+                string[] Response6 = Response2.Split('-');
+                Number = Number - double.Parse(Response6[1]);
+            }
+            else if (Response2.Contains("*"))
+            {
+                string[] Response6 = Response2.Split('*');
+                Number = Number * double.Parse(Response6[1]);
+            }
+            else if (Response2.Contains("/"))
+            {
+                string[] Response6 = Response2.Split('/');
+                Number = Number / double.Parse(Response6[1]);
+            }
+            else
+            {
+                Console.WriteLine("Enter a correct value...");
+            }
+            return Number;
+        }
+        public override string ToString()
+        {
+            return "If you need help press: 1\n" + Number + " ";
+        }
+    }
+}
